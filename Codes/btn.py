c2ff2604be5ed029469b1855cdedb20dfbd1582c
Gabriela Ramos <gabi19ramos@gmail.com>
2017-05-23 15:27:24 -0300

    Códigos básicos referentes aos sensores

---
+++ Codes/btn.py
@@ -0,0 +1,12 @@
+from libsoc_zero.GPIO import Button
+from time import sleep
+
+btn = Button('GPIO-A')
+
+while True:
+    sleep(0.25)
+    if btn.is_pressed():
+        print('Button is pressed!')
+    else:
+        print('Button is not pressed!')
+
